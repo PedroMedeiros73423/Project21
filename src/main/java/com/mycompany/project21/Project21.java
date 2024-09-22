632c31dc47d6b9d8c55a3749e3aec9c684c72892
PedroMedeiros7631 <81970006+PedroMedeiros73423@users.noreply.github.com>
2024-09-22 19:26:16 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/project21/Project21.java
@@ -0,0 +1,11 @@
+package com.mycompany.project21;
+
+public class Project21 {
+
+    public static void main(String[] args) {
+        for(int i = 34; i < 57; i = i + 2){
+            System.out.println("É par: " + i);
+        }
+        
+    }
+}
